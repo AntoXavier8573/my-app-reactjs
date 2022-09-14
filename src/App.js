4bfda1cf54509cd9281057192e6e225016ff6491
Manikandan Anbalagan <manikandan.a@analyticbrains.com>
2022-09-14 13:40:04 +0530

    changed author name

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       Happy coding!!!
-      With new Branch_new1 and Branch_new2.
+      With new Branch_new1 and changed author name.
     </div>
   );
 }
