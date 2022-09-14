fba8e2725fbe6f74958a4c2019002e60c8f529af
Xavier <manikandan.a@analyticbrains.com>
2022-09-14 13:43:09 +0530

    changed name

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       Happy coding!!!
-      With new Branch_new1 and changed author name.
+      With new Branch_new1 and changed now author name.
     </div>
   );
 }
